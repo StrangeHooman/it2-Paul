57c4d925aa8636f86b48d76f004d8a219c31bf60
StrangeHooman <paul.zieritz@gmail.com>
2026-09-01 15:40:08 +0200

    Abstraktifiserte koden
    - Gjorde det mulig å velge poenggrense for å vinne.
    
    - Gjorde det mulig å velge selv hivlke hender som finnes i spillet, og den finner selv ut av vinnende kombinasjoner.

--- general/steinSaksPapir.py
+++ general/steinSaksPapir.py
@@ -1,32 +1,55 @@
 import random
+import time
 
-choices = ["stein", "saks", "papir"]
+choices = ["stein", "saks", "papir", "øgle", "spock", "kaktus", "bønne"]
 
 win = 0
 loose = 0
 
+scoreLim = 3
 
-while loose < 3 and win < 3:
-    choice = str(input("Velg hånd: "))
-    computer = random.randint(0, 2)
+
+while loose < scoreLim and win < scoreLim:
+    choice = str(input(f"---------------------\nVelg en hånd: \nMulige valg {choices}\n\nDitt valg: "))
+
+    time.sleep(0.5)
+
+    # availableChoices = [].append(choices[(choice - 1) % 3])
+
+    choice.index(choice)
+
+    computer = random.randint(0, len(choices)-1)
 
     try:
         choice = choices.index(choice)
-        print("Motstander valgte ", choices[computer])
+        print("Motstander valgte: ", choices[computer])
 
-        if choice == ((computer + 1)%3):
+        if choice == computer:
+            print("Uavgjort\n")
+            time.sleep(1.5)
+
+        elif abs(choice - computer) % 2 == 0:
             loose += 1
             print("Du tapte runden\n")
+            time.sleep(1.5)
 
-        elif choice == ((computer - 1)%3):
+        elif abs(choice - computer) % 2 != 0:
             win += 1
             print("Du vant runden\n")
+            time.sleep(1.5)
 
         else:
-            print("Uavgjort\n")
+            print("Wut da hell?")
 
-    except KeyError:
-        print("Ugyldig input!")
+    except ValueError:
+        if choice == "pistol":
+            win += 1
+            print("Light 'em up\n")
+            time.sleep(1.5)
+
+        else:
+            print("Ugyldig input!\n")
+            time.sleep(1.5)
 
 
 if win > loose:
